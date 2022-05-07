c44cbddba66bc908ba8c170fbd0b660d6659077d
MatthewNgan <58377002+MatthewNgan@users.noreply.github.com>
2022-05-07 18:12:12 +0800

    Enabled search function on default. Added guideline for using own api key.

--- pages/api/searchchannel.js
+++ pages/api/searchchannel.js
@@ -5,14 +5,14 @@ export default async function handler(req, res) {
   const Q = JSON.parse(req.body).term;
 
   // uncomment the below for production
-  // const fetch_url = `https://www.googleapis.com/youtube/v3/search?q=${Q}&key=${KEY}&part=snippet&type=channel&maxResults=15`;
-  // const api_res = await fetch(fetch_url);
-  // const data = await api_res.json();
+  const fetch_url = `https://www.googleapis.com/youtube/v3/search?q=${Q}&key=${KEY}&part=snippet&type=channel&maxResults=15`;
+  const api_res = await fetch(fetch_url);
+  const data = await api_res.json();
 
   // for dev only:
   // using local file instead of real data
   // to reduce the usage of the quota
-  const data = require('./loc_res.json')
+  // const data = require('./loc_res.json')
 
   
   res.json(data);
